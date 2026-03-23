ddb3aaff146aff8ff40b080bbe82ef696a0d8b27
Joseph Bochinski <stirgejr@gmail.com>
2026-03-23 13:23:54 -0400

    ui HUD updates

---
+++ aetherglow-ui/src/app/pages/hud/hud.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hud',
+  imports: [],
+  templateUrl: './hud.html',
+  styleUrl: './hud.scss',
+})
+export class Hud {
+
+}
