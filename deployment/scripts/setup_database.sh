728f97f000c960b777d279fb1217f75bec8e0dbc
Joseph Bochinski <stirgejr@gmail.com>
2026-03-25 20:03:16 -0400

    scripts

---
+++ deployment/scripts/setup_database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+_check_install() {
+	if [ -z "$(command -v "createdb")" ]; then
+		sudo apt install -y postgresql
+	fi
+}
+
+_create-db() {
+	createdb trutrafik
+}
+
+_check_install
+_create_db
