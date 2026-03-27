e2b722a1389f8124c520d0f10ab44a5837409614
Joseph Bochinski <stirgejr@gmail.com>
2026-03-27 06:23:37 -0400

    debugging

--- magmek_backend/magmek_backend/traffic/traffic_utils.py
+++ magmek_backend/magmek_backend/traffic/traffic_utils.py
@@ -3,7 +3,7 @@ from geoalchemy2.elements import WKTElement
 from magmek_backend.traffic.tr_models import SlVector
 
 
-def to_wkt(vector: SlVector, has_z=False):
+def to_wkt(vector: SlVector, has_z=True):
     if has_z:
         return WKTElement(f"POINT({vector.x} {vector.y} {vector.z})", srid=0)
     return WKTElement(f"POINT({vector.x} {vector.y})", srid=0)
