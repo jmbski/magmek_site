48143ce4ab1d09818d6bf09beb7c5f50d332c5de
Joseph Bochinski <stirgejr@gmail.com>
2026-03-30 10:30:13 -0400

    traffic

--- magmek-frontend/projects/true-traffic/src/app/pages/home/home.ts
+++ magmek-frontend/projects/true-traffic/src/app/pages/home/home.ts
@@ -3,11 +3,16 @@ import { TtPageLayout } from '@tt-app/layout';
 import { SimSnapshot } from '@tt-app/models';
 import { TrafficTxService } from '@tt-app/services';
 import { SimSnapshots } from '../../components/sim-snapshots/sim-snapshots';
+import { Select, SelectItem } from 'primeng/select';
+import { WeakObj } from '@shared/typing';
+import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'tt-home',
     imports: [
         TtPageLayout,
         SimSnapshots,
+        Select,
+        FormsModule,
     ],
     templateUrl: './home.html',
     styleUrl: './home.scss',
@@ -18,6 +23,17 @@ export class TtHome {
     public simSnapshots = model<SimSnapshot[]>([]);
 
     public simName: string = 'WelcomeHubSandbox';
+
+    public dropdownOptions: WeakObj[] = [
+        {
+            name: 'WelcomeHubSandbox',
+            value: 'WelcomeHubSandbox',
+        },
+        {
+            name: 'Hill Valley',
+            value: 'Hill Valley',
+        },
+    ];
 
     constructor() {
         this.txSvc.getSimSnapshots(this.simName).then(response => {
