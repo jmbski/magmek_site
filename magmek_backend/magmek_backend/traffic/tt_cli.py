8834d608ad5f3e5d93c6812d43a2fc7eff32aa97
Joseph Bochinski <stirgejr@gmail.com>
2026-04-29 12:54:05 -0400

    tt-cli updates

--- magmek_backend/magmek_backend/traffic/tt_cli.py
+++ magmek_backend/magmek_backend/traffic/tt_cli.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 from datetime import datetime
 
@@ -6,6 +7,15 @@ import argcomplete
 
 from jbutils import jbutils, JbuConsole
 from ptpython import embed
+from sqlalchemy import select, desc
+
+from magmek_backend.traffic.sql_conn import get_db, SessionLocal
+from magmek_backend.traffic.entities import (
+    DbAvatar,
+    DbAvatarSnapshot,
+    DbSim,
+    DbSimSnapshot,
+)
 
 DEF_START_DATE = datetime.fromtimestamp(1773979200)  # March 20 2026 00:00:00
 DEF_END_DATE = datetime.fromtimestamp(1774584000)  # March 27 2026 00:00:00
@@ -34,3 +44,35 @@ data_parser.add_argument("--end-date", "-e", default=DEF_END_DATE)
 data_parser.add_argument(
     "--interval", "-I", type=int, default=60, help="Time interval to add data"
 )
+
+args = parser.parse_args()
+
+
+def db_test():
+    db = next(get_db())
+    query = (
+        select(DbSimSnapshot)
+        .where(DbSimSnapshot.sim_name == "Lunar Haven")
+        .order_by(desc(DbSimSnapshot.ts))
+        .limit(1)
+    )
+
+    result = db.execute(query).scalar_one_or_none()
+
+    db.close()
+    return result
+
+
+def main() -> None:
+    """Main function"""
+
+    if args.interactive:
+        sys.exit(
+            embed(
+                globals=globals(), locals=locals(), history_filename="tt_cli.hist"
+            )
+        )
+
+
+if __name__ == "__main__":
+    main()
