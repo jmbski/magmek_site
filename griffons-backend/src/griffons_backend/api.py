0f9c1c3bcc61d99deeaa4d8b76e966c5dfba0b11
Joseph Bochinski <stirgejr@gmail.com>
2026-09-02 12:32:11 -0400

    endpoint testing

--- griffons-backend/src/griffons_backend/api.py
+++ griffons-backend/src/griffons_backend/api.py
@@ -16,7 +16,7 @@ def get_router() -> APIRouter:
     )
     def get_calendar_events(logger: ApiLogger):
         logger.info("Events endpoint reached.")
-        return service.get_upcoming_events()
+        return "Test"  # service.get_upcoming_events()
 
     @router.get(f"{consts.BASE_URL}/health")
     def health(logger: ApiLogger):
