75b733d91913c007f830793e4b814929342b70f8
Joseph Bochinski <stirgejr@gmail.com>
2026-09-02 12:45:11 -0400

    endpoint testing

--- griffons-backend/src/griffons_backend/api.py
+++ griffons-backend/src/griffons_backend/api.py
@@ -10,7 +10,7 @@ from griffons_backend.models import CalendarEventModel
 def get_router() -> APIRouter:
     router = APIRouter()
 
-    @router.get(f"{consts.BASE_URL}/calendarevents")
+    @router.get(f"{consts.BASE_URL}/calendar-events")
     def get_calendar_events(logger: ApiLogger):
         logger.info("Events endpoint reached.")
         return "Test"  # service.get_upcoming_events()
