23f3a77860cc7add52c97fa98fceae26c1167ed8
Joseph Bochinski <stirgejr@gmail.com>
2026-09-02 12:03:48 -0400

    typo

--- griffons-backend/src/griffons_backend/consts.py
+++ griffons-backend/src/griffons_backend/consts.py
@@ -14,7 +14,7 @@ CALENDAR_AUTH_PATH = os.environ.get("GRIFFONS_GOOGLE_CALENDAR_AUTH_FILE", "")
 GRIFFONS_DB_PATH = os.environ.get("GRIFFONS_DB_PATH", "")
 
 BASE_URL = "/api/v1"
-LOG_DIR = Path("/var/logs")
+LOG_DIR = Path("/var/log")
 LOG_FMT_ID = "[%(name)s::%(module)s::%(origFunc)s]"
 LOG_FMT_RICH = f"{LOG_FMT_ID} %(message)s"
 LOG_FMT_STND = f"%(asctime)s {LOG_FMT_ID} [%(levelname)s] %(message)s"
